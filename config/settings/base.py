8c64fac18bef14da04af6665c9f315784e4ea545
pando <ndjman7@gmail.com>
2019-01-14 17:14:53 +0900

    Add user info field

--- config/settings/base.py
+++ config/settings/base.py
@@ -72,6 +72,7 @@ THIRD_PARTY_APPS = [
 LOCAL_APPS = [
     'pandogram.users.apps.UsersAppConfig',
     # Your stuff: custom apps go here
+    'pandogram.images.apps.ImagesConfig',
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
