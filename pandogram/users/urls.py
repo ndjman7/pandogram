14ee2262c466f911629e625a47325e5fb7e8cb1a
pando <ndjman7@gmail.com>
2019-01-22 13:49:35 +0900

    Add exploreing people to follow api

--- pandogram/users/urls.py
+++ pandogram/users/urls.py
@@ -1,16 +1,13 @@
-from django.urls import path
+from django.conf.urls import url
 
-from pandogram.users.views import (
-    user_list_view,
-    user_redirect_view,
-    user_update_view,
-    user_detail_view,
-)
+from . import views
+
 
 app_name = "users"
 urlpatterns = [
-    path("", view=user_list_view, name="list"),
-    path("~redirect/", view=user_redirect_view, name="redirect"),
-    path("~update/", view=user_update_view, name="update"),
-    path("<str:username>/", view=user_detail_view, name="detail"),
+    url(
+        regex=r"^explore/$",
+        view=views.ExploreUsers.as_view(),
+        name="explore_users"
+    ),
 ]
