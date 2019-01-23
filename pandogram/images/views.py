31a95022c976afeab2111e99bd91e3d96ca7c1d8
pando <ndjman7@gmail.com>
2019-01-23 16:52:53 +0900

    Add delete method in ImageDetail api

--- pandogram/images/views.py
+++ pandogram/images/views.py
@@ -190,14 +190,20 @@ class ModerateComment(APIView):
 
 class ImageDetail(APIView):
 
-    def get(self, request, image_id, format=None):
+    def find_own_image(self, image_id, user):
 
-        user = request.user
+        try:
+            image = models.Image.objects.get(id=image_id, creator=user)
+            return image
+        except models.Image.DoesNotExist:
+            return None
+
+    def get(self, request, image_id, format=None):
 
         try:
             image = models.Image.objects.get(id=image_id)
         except models.Image.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND())
+            return Response(status=status.HTTP_404_NOT_FOUND)
 
         serializer = serializers.ImageSerializer(image)
 
@@ -207,9 +213,10 @@ class ImageDetail(APIView):
 
         user = request.user
 
-        try:
-            image = models.Image.objects.get(id=image_id, creator=user)
-        except models.Image.DoesNotExist:
+        image = self.find_own_image(image_id, user)
+
+        if image is None:
+
             return Response(status=status.HTTP_401_UNAUTHORIZED)
 
         # partial은 serializer에서 모든 필드를 수정하지 않고 몇개의 필드만 수정가능하도록 함
@@ -225,3 +232,17 @@ class ImageDetail(APIView):
         else:
 
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, image_id, format=None):
+
+        user = request.user
+
+        image = self.find_own_image(image_id, user)
+
+        if image is None:
+
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
+
+        image.delete()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
